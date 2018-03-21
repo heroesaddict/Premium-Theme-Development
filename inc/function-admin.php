787edbb32d089237c8e6c5572b05d992e3310d29
heroesaddict <chattokim@gmail.com>
2018-03-21 08:14:43 +0800

    Part1 & Part2

---
+++ inc/function-admin.php
@@ -0,0 +1,20 @@
+<?php 
+
+/*
+@package sunsettheme
+=================================
+		ADMIN PAGE
+=================================
+*/
+
+function sunset_add_admin_page() {
+	add_menu_page('Sunset Them Options', 'Sunset', 'manage_options', 'alecaddd-sunset', 'sunset_theme_create_page', /*get_template_directory_uri() . '/img/sunset-icon.png'*/'dashicons-admin-customizer', 110);
+}
+
+add_action('admin_menu', 'sunset_add_admin_page');
+
+function sunset_theme_create_page() {
+	//generation of our admin page
+	echo '<h1>Sunset Theme Options</h1>';
+
+} 
