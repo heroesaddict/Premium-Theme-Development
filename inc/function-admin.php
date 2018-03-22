6f6b345e5fcc190c9fa8db5b70430ca2bc56c74e
heroesaddict <chattokim@gmail.com>
2018-03-22 10:03:23 +0800

    Part4

--- inc/function-admin.php
+++ inc/function-admin.php
@@ -13,9 +13,25 @@ function sunset_add_admin_page() {
 	//Generate Sunset Admin Sub Pages
 	add_submenu_page( 'alecaddd_sunset', 'Sunset Theme Options', 'General', 'manage_options', 'alecaddd_sunset','sunset_theme_create_page');
 	add_submenu_page( 'alecaddd_sunset', 'Sunset CSS Options', 'Custom CSS', 'manage_options', 'alecaddd_sunset_css','sunset_theme_settings_page');
+	//Activate Custom Settings
+	add_action('admin_init', 'sunset_custom_settings');
 }
-
 add_action('admin_menu', 'sunset_add_admin_page');
+
+function sunset_custom_settings() {
+	register_setting( 'sunset-settings-group', 'first_name' );
+	add_settings_section( 'sunset-sidebar-options', 'Sidebar Options', 'sunset_sidebar_options', 'alecaddd_sunset' );
+	add_settings_field( 'sidebar-name', 'First Name', 'sunset_sidebar_name', 'alecaddd_sunset', 'sunset-sidebar-options' );
+}
+function sunset_sidebar_name() {
+	$firstName = esc_attr(get_option('first_name'));
+	echo '<input type="text" name="first_name" value="'.$firstName.'" placeholder="First Name"/>';
+} 
+
+function sunset_sidebar_options() {
+	echo 'Customize your Sidebar Information';
+} 
+
 function sunset_theme_create_page() {
 	//generation of our admin page
 	require_once(get_template_directory() . '/inc/templates/sunset-admin.php');
--- inc/templates/sunset-admin.php
+++ inc/templates/sunset-admin.php
@@ -1,4 +1,9 @@
 <h1>Sunset Theme Options</h1>
-<h3 class="title">Manage Options</h3>
-<p>Customize the default Wordpress Appearance Options</p>
-<?php bloginfo( 'name' ) ?>
+<?php settings_errors(); ?>
+<form method="post" action="options.php">
+	<?php settings_fields( 'sunset-settings-group' ); ?>
+	<?php do_settings_sections( 'alecaddd_sunset' ); ?>
+	<?php submit_button(); ?>
+</form>
+
+
