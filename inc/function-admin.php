d22e41064b3cddd07e6f7ae97ea3f13b0439af63
heroesaddict <chattokim@gmail.com>
2018-03-26 11:00:13 +0800

    Part7

--- inc/function-admin.php
+++ inc/function-admin.php
@@ -19,6 +19,7 @@ function sunset_add_admin_page() {
 add_action('admin_menu', 'sunset_add_admin_page');
 
 function sunset_custom_settings() {
+	register_setting( 'sunset-settings-group', 'profile_picture' );
 	register_setting( 'sunset-settings-group', 'first_name' );
 	register_setting( 'sunset-settings-group', 'last_name' );
 	register_setting( 'sunset-settings-group', 'user_description' );
@@ -27,13 +28,18 @@ function sunset_custom_settings() {
 	register_setting( 'sunset-settings-group', 'gplus_handler' );
 	
 	add_settings_section( 'sunset-sidebar-options', 'Sidebar Option', 'sunset_sidebar_options', 'alecaddd_sunset');
-	
+
+	add_settings_field( 'sidebar-profile-picture', 'Profile Picture', 'sunset_sidebar_profile', 'alecaddd_sunset', 'sunset-sidebar-options');
 	add_settings_field( 'sidebar-name', 'Full Name', 'sunset_sidebar_name', 'alecaddd_sunset', 'sunset-sidebar-options');
 	add_settings_field( 'sidebar-description', 'Description', 'sunset_sidebar_description', 'alecaddd_sunset', 'sunset-sidebar-options');
 	add_settings_field( 'sidebar-twitter', 'Twitter handler', 'sunset_sidebar_twitter', 'alecaddd_sunset', 'sunset-sidebar-options');
 	add_settings_field( 'sidebar-facebook', 'Facebook handler', 'sunset_sidebar_facebook', 'alecaddd_sunset', 'sunset-sidebar-options');
 	add_settings_field( 'sidebar-gplus', 'Google+ handler', 'sunset_sidebar_gplus', 'alecaddd_sunset', 'sunset-sidebar-options');
 }
+function sunset_sidebar_profile() {
+	$picture = esc_attr(get_option('profile_picture'));
+	echo '<input type="button" class="button button-secondary" value="Upload Profile Picture" id="upload-button"/> <input type="hidden" name="profile_picture" id="profile-picture" value="'.$picture.'" />';
+} 
 function sunset_sidebar_name() {
 	$firstName = esc_attr(get_option('first_name'));
 	$lastName = esc_attr(get_option('last_name'));
