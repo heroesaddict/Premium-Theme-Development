57b9b34ebcd7af073f2a53dfea1f1936c012ab81
heroesaddict <chattokim@gmail.com>
2018-03-31 14:57:26 +0800

    Part11.

--- inc/custom-post-type.php
+++ inc/custom-post-type.php
@@ -10,6 +10,8 @@
 $contact = get_option( 'activate_contact' );
 if( @$contact == 1 ) {
 	add_action('init', 'sunset_contact_custom_post_type' );
+	add_filter('manage_sunset-contact_posts_columns', 'sunset_set_contact_columns')	;
+	add_action('manage_sunset-contact_posts_custom_column', 'sunset_contact_custom_column', 10, 2);
 }
 /* CONTACT CPT */
 function sunset_contact_custom_post_type (){
@@ -31,4 +33,23 @@ function sunset_contact_custom_post_type (){
 	);
 
 	register_post_type('sunset-contact', $args);
+}
+function sunset_set_contact_columns($columns){
+	$newColumns = array();
+	$newColumns['title'] = 'Full Name';
+	$newColumns['message'] = 'Message';
+	$newColumns['email'] = 'Email';
+	$newColumns['date'] = 'Date';
+	return $newColumns;
+}
+function sunset_contact_custom_column($column, $post_id){
+	switch( $column ){
+		case 'message' :
+			echo get_the_excerpt();
+			break;
+		case 'email' :
+			//email column
+		echo 'My sample email';
+			break;
+	}
 }
