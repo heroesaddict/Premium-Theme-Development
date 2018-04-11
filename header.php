7628ad38fa6606a0b77e3ef40ce985d52fd9f3ab
heroesaddict <chattokim@gmail.com>
2018-04-12 05:23:29 +0800

    Part17

--- header.php
+++ header.php
@@ -27,7 +27,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-12">
-				<div class="header-container background-image text-center" style="background-image: url(<?php header_image(); ?>);">
+				<header class="header-container background-image text-center" style="background-image: url(<?php header_image(); ?>);">
 					<div class="header-content table">
 						<div class="table-cell">
 							<h1 class="site-title sunset-icon">
@@ -37,12 +37,43 @@
 							<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 						</div><!-- .table-cell -->
 					</div><!-- .header-content -->
-					<div class="nav-container"></div><!-- .nav-container -->
-				</div><!-- .header-container -->
+					<div class="nav-container">
+						<nav class="navbar navbar-default navbar-sunset">
+							<?php 
+								wp_nav_menu(array(
+									'theme_location' => 'primary',
+									'container' => false,
+									'menu_class' => 'nav navbar-nav'
+
+								));
+							?>
+						</nav>
+					</div><!-- .nav-container -->
+				</header><!-- .header-container -->
 				
 			</div><!-- .col-xs-12 -->
 		</div><!-- .row -->
 	</div><!-- .container -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
