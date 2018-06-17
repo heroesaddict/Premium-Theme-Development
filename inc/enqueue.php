f8e734226fb2513dcd1887566eb1766348e59d4e
heroesaddict <chattokim@gmail.com>
2018-06-17 22:28:22 +0800

    Part54

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -23,23 +23,32 @@ function sunset_load_admin_scripts( $hook ){
 		'sunset_page_alecaddd_sunset_theme',
 		'sunset_page_alecaddd_sunset_theme_contact'
 	);
-	//if( 'toplevel_page_alecaddd_sunset' == $hook ){
-	if( in_array($hook, $pages_array)){
-		wp_enqueue_style('raleway-admin');
-		wp_enqueue_style( 'sunset_admin' );
+	
+	//PHP 7
+	
+	if( in_array( $hook, $pages_array ) ){
 		
-	} else if('toplevel_page_alecaddd_sunset' == $hook) {
+		wp_enqueue_style( 'raleway-admin' );
+		wp_enqueue_style( 'sunset_admin' );
+	
+	} 
+	
+	if( 'toplevel_page_alecaddd_sunset' == $hook ){
+		
 		wp_enqueue_media();
-		wp_enqueue_script('sunset-admin-script');
-	} else if('sunset_page_alecaddd_sunset_css' ==$hook) {
-		wp_enqueue_style('raleway-admin');
-		wp_enqueue_style( 'sunset_admin' );
 		
-		wp_enqueue_style( 'ace', get_template_directory_uri() . '/css/sunset.ace.css', array(), '1.0.0', 'all');
-		wp_enqueue_script('ace',  get_template_directory_uri() . '/js/ace/ace.js', array('jquery'), '1.2.1', true);
-		wp_enqueue_script('sunset-custom-css-script',  get_template_directory_uri() . '/js/sunset-custom_css.js', array('jquery'), '1.2.1', true);
-	} else { return; }
-
+		wp_enqueue_script( 'sunset-admin-script' );
+		
+	}
+	
+	if ( 'sunset_page_alecaddd_sunset_css' == $hook ){
+		
+		wp_enqueue_style( 'ace', get_template_directory_uri() . '/css/sunset.ace.css', array(), '1.0.0', 'all' );
+		
+		wp_enqueue_script( 'ace', get_template_directory_uri() . '/js/ace/ace.js', array('jquery'), '1.2.1', true );
+		wp_enqueue_script( 'sunset-custom-css-script', get_template_directory_uri() . '/js/sunset.custom_css.js', array('jquery'), '1.0.0', true );
+	
+	}
 	
 }
 add_action( 'admin_enqueue_scripts', 'sunset_load_admin_scripts' );
